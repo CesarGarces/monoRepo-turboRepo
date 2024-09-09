29d4e406fe0a45325e4f3f893600671b9c70448d
cesar garces <cesargarce@gmail.com>
2024-09-08 23:03:45 -0500

    config linter

--- packages/proyecto2/.eslintrc.js
+++ packages/proyecto2/.eslintrc.js
@@ -1,10 +1,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/react-internal.js"],
+  extends: ["@repo/eslint-config/react-internal"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
     tsconfigRootDir: __dirname,
   },
+  ignorePatterns: ["dist"],
 };
